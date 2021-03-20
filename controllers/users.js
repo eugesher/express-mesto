bd29cd4140b50336b85b2fa7991ac9a420fd32e9
Evgenii Chernyshov <mailbox@eugesher.site>
2021-03-20 15:57:11 +0300

    feat: users controller

---
+++ controllers/users.js
@@ -0,0 +1,21 @@
+const User = require('../models/user');
+
+module.exports.getUsers = (request, response) => {
+  User.find({})
+    .then((users) => response.send({ data: users }))
+    .catch((error) => response.status(500).send({ message: error.message }));
+};
+
+module.exports.getUserById = (request, response) => {
+  User.findById(request.params._id)
+    .then((user) => response.send({ data: user }))
+    .catch((error) => response.status(500).send({ message: error.message }));
+};
+
+module.exports.createUser = (request, response) => {
+  const { name, about, avatar } = request.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => response.send({ data: user }))
+    .catch((error) => response.status(500).send({ message: error.message }));
+};
