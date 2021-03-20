ab4d9887fa5dcbe15ff2e70de6923bcaa8772236
Evgenii Chernyshov <mailbox@eugesher.site>
2021-03-20 15:04:24 +0300

    feat: connect to db

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
+
+app.listen(PORT);
