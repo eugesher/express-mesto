db2be94aa9aa18de3d3ae9329142a1e85455a4a9
Evgenii Chernyshov <mailbox@eugesher.site>
2021-04-01 00:56:23 +0300

    feat: add email and password to user schema; add default name, about, avatar values

--- controllers/users.js
+++ controllers/users.js
@@ -22,9 +22,13 @@ module.exports.getUserById = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
+  const {
+    email, password, name, about, avatar,
+  } = req.body;
 
-  User.create({ name, about, avatar })
+  User.create({
+    email, password, name, about, avatar,
+  })
     .then((user) => res.send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') handleValidationError(err, res);
--- models/user.js
+++ models/user.js
@@ -1,21 +1,35 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: validator.isEmail,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
   name: {
     type: String,
     minlength: [2, 'Поле \'имя\' должно содержать не менее 2 символов'],
     maxlength: [30, 'Поле \'имя\' должно содержать не более 30 символов'],
-    required: [true, 'Поле \'имя\' не может быть пустым'],
+    required: false,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     minlength: [2, 'Поле \'о себе\' должно содержать не менее 2 символов'],
     maxlength: [30, 'Поле \'о себе\' должно содержать не более 30 символов'],
-    required: [true, 'Поле \'о себе\' не может быть пустым'],
+    required: false,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: [true, 'Поле \'аватар\' не может быть пустым'],
+    required: false,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
 });
 
