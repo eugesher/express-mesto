c25b733a0e5f807eac13a748527e0c82621fe286
Evgenii Chernyshov <mailbox@eugesher.site>
2021-04-01 18:03:03 +0300

    feat: auth middleware

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const router = require('./routes');
 const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const router = require('./routes');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -17,9 +18,10 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(router);
 app.post('/signin', login);
 app.post('/signup', createUser);
+app.use(auth);
+app.use(router);
 app.use((req, res) => {
   res.status(404).send({ message: `Ресурс по адресу ${req.path} не найден` });
 });
