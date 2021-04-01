225c1e24d88b609a4fe162b6df34f18fbd20bc84
Evgenii Chernyshov <mailbox@eugesher.site>
2021-04-01 16:24:11 +0300

    feat: users login controller

--- controllers/users.js
+++ controllers/users.js
@@ -77,3 +77,26 @@ module.exports.updateUserAvatar = (req, res) => {
       else handleServerError(err, res);
     });
 };
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password);
+    })
+    .then((matched) => {
+      if (!matched) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+      res.send({ message: `Выполнен вход: ${email}` });
+    })
+    .catch((err) => {
+      res
+        .status(401)
+        .send({ message: err.message });
+    });
+};
