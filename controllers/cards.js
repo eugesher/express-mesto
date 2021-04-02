9b893c772ff6a52fff1beeca0b7c0f8e8eb849d0
Evgenii Chernyshov <mailbox@eugesher.site>
2021-04-02 19:19:13 +0300

    feat: handle cards controller errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,62 +1,61 @@
 const Card = require('../models/card');
+const NotFoundError = require('../errors/not-found-error');
+const BadRequestError = require('../errors/bad-request-error');
+const ForbiddenError = require('../errors/forbidden-error');
 const {
   handleValidationError,
-  handleCastError,
-  handleNotFoundError,
-  handleServerError,
-  handleDeleteNotOwnCardError,
 } = require('../utils');
 
-module.exports.getCards = (req, res) => {
+module.exports.getCards = (req, res, next) => {
   Card.find({})
     .then((cards) => res.send(cards))
-    .catch((err) => handleServerError(err, res));
+    .catch(next);
 };
 
-module.exports.createCard = (req, res) => {
+module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') handleValidationError(err, res);
-      else handleServerError(err, res);
+      next(err);
     });
 };
 
-module.exports.deleteCard = (req, res) => {
+module.exports.deleteCard = (req, res, next) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then((card) => {
-      if (!card) handleNotFoundError(res);
-      else if (!card.owner.equals(req.user._id)) handleDeleteNotOwnCardError(res);
+      if (!card) throw new NotFoundError('Карточки не существует');
+      else if (!card.owner.equals(req.user._id)) throw new ForbiddenError('Можно удалять только свои карточки');
       else res.send(card);
     })
     .catch((err) => {
-      if (err.kind === 'ObjectId') handleCastError(err, res);
-      else handleServerError(err, res);
+      if (err.kind === 'ObjectId') next(new BadRequestError('Недопустимый идентификатор карточки'));
+      next(err);
     });
 };
 
-module.exports.likeCard = (req, res) => {
+module.exports.likeCard = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
     .then((card) => {
-      if (!card) handleNotFoundError(res);
+      if (!card) throw new NotFoundError('Карточки не существует');
       else res.send(card);
     })
     .catch((err) => {
-      if (err.kind === 'ObjectId') handleCastError(err, res);
-      else handleServerError(err, res);
+      if (err.kind === 'ObjectId') next(new BadRequestError('Недопустимый идентификатор карточки'));
+      next(err);
     });
 };
 
-module.exports.dislikeCard = (req, res) => {
+module.exports.dislikeCard = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
     .then((card) => {
-      if (!card) handleNotFoundError(res);
+      if (!card) throw new NotFoundError('Карточки не существует');
       else res.send(card);
     })
     .catch((err) => {
-      if (err.kind === 'ObjectId') handleCastError(err, res);
-      else handleServerError(err, res);
+      if (err.kind === 'ObjectId') next(new BadRequestError('Недопустимый идентификатор карточки'));
+      next(err);
     });
 };
