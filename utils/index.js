2d07d7a0e3ed5b29c7dcb3d6fdfb8f16e9e402da
Evgenii Chernyshov <mailbox@eugesher.site>
2021-04-03 13:01:58 +0300

    refactor: remove unused utils

--- utils/index.js
+++ utils/index.js
@@ -5,23 +5,3 @@ module.exports.handleValidationError = (err, res) => {
       .join('. '),
   });
 };
-
-module.exports.handleDuplicateEmailError = (res) => {
-  res.status(409).send({ message: 'Пользователь с таким email уже существует.' });
-};
-
-module.exports.handleCastError = (res) => {
-  res.status(400).send({ message: 'Недопустимый идентификатор' });
-};
-
-module.exports.handleNotFoundError = (res) => {
-  res.status(404).send({ message: 'Запрашиваемый ресурс не найден.' });
-};
-
-module.exports.handleServerError = (res) => {
-  res.status(500).send({ message: 'На сервере произошла ошибка' });
-};
-
-module.exports.handleDeleteNotOwnCardError = (res) => {
-  res.status(403).send({ message: 'Можно удалять только свои карточки' });
-};
